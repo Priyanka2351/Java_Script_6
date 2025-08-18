231a447fcbf8631f02b08a7e0a84d74051306621
Priyanka <145449555+Priyanka2351@users.noreply.github.com>
2025-08-18 14:00:17 +0000

    string and memory

---
+++ js_basic/03_string.js
@@ -0,0 +1,183 @@
+// string
+const name="kaushalya devi"
+const age= 51
+console.log(name+age); // it's old one 
+console.log(`Name is ${name} and her age is ${age}`); // back ticks (``)
+// there are methodin string 
+const pp= new String('happy bday')
+console.log( pp.length);
+
+console.log(pp.__proto__);
+console.log( pp.charAt(4));
+console.log( pp.indexOf("y"));
+console.log( pp.substring(0,5));
+console.log( pp.slice(-6,-0));
+let s="    this is good ht kn  jndb jjnn"
+console.log( s.trim());
+let url="http://priyanka.com/gogo%201priyanka"
+console.log(url.replace(201,"pogo"))
+console.log(url.includes(201));
+console.log(url.includes("pogo"));
+console.log(s.split(" "));
+// [[Prototype]]
+// : 
+// String
+// anchor
+// : 
+// ƒ anchor()
+// at
+// : 
+// ƒ at()
+// big
+// : 
+// ƒ big()
+// blink
+// : 
+// ƒ blink()
+// bold
+// : 
+// ƒ bold()
+// charAt
+// : 
+// ƒ charAt()
+// charCodeAt
+// : 
+// ƒ charCodeAt()
+// codePointAt
+// : 
+// ƒ codePointAt()
+// concat
+// : 
+// ƒ concat()
+// constructor
+// : 
+// ƒ String()
+// endsWith
+// : 
+// ƒ endsWith()
+// fixed
+// : 
+// ƒ fixed()
+// fontcolor
+// : 
+// ƒ fontcolor()
+// fontsize
+// : 
+// ƒ fontsize()
+// includes
+// : 
+// ƒ includes()
+// indexOf
+// : 
+// ƒ indexOf()
+// isWellFormed
+// : 
+// ƒ isWellFormed()
+// italics
+// : 
+// ƒ italics()
+// lastIndexOf
+// : 
+// ƒ lastIndexOf()
+// length
+// : 
+// 0
+// link
+// : 
+// ƒ link()
+// localeCompare
+// : 
+// ƒ localeCompare()
+// match
+// : 
+// ƒ match()
+// matchAll
+// : 
+// ƒ matchAll()
+// normalize
+// : 
+// ƒ normalize()
+// padEnd
+// : 
+// ƒ padEnd()
+// padStart
+// : 
+// ƒ padStart()
+// repeat
+// : 
+// ƒ repeat()
+// replace
+// : 
+// ƒ replace()
+// replaceAll
+// : 
+// ƒ replaceAll()
+// search
+// : 
+// ƒ search()
+// slice
+// : 
+// ƒ slice()
+// small
+// : 
+// ƒ small()
+// split
+// : 
+// ƒ split()
+// startsWith
+// : 
+// ƒ startsWith()
+// strike
+// : 
+// ƒ strike()
+// sub
+// : 
+// ƒ sub()
+// substr
+// : 
+// ƒ substr()
+// substring
+// : 
+// ƒ substring()
+// sup
+// : 
+// ƒ sup()
+// toLocaleLowerCase
+// : 
+// ƒ toLocaleLowerCase()
+// toLocaleUpperCase
+// : 
+// ƒ toLocaleUpperCase()
+// toLowerCase
+// : 
+// ƒ toLowerCase()
+// toString
+// : 
+// ƒ toString()
+// toUpperCase
+// : 
+// ƒ toUpperCase()
+// toWellFormed
+// : 
+// ƒ toWellFormed()
+// trim
+// : 
+// ƒ trim()
+// trimEnd
+// : 
+// ƒ trimEnd()
+// trimLeft
+// : 
+// ƒ trimStart()
+// trimRight
+// : 
+// ƒ trimEnd()
+// trimStart
+// : 
+// ƒ trimStart()
+// valueOf
+// : 
+// ƒ valueOf()
+// Symbol(Symbol.iterator)
+// : 
+// ƒ [Symbol.iterator]()
---
+++ js_basic/memory.js
@@ -0,0 +1,10 @@
+// memory storage
+// there are two type of 
+// -> stack
+// -> heap
+
+// All primitive datatype is store in the stack memory in which we use the LIFO if we want to change and updata the
+// value than it's original value is not efficted...it's provide the copy of value 
+
+// All non-primitive datatype is store in the heap memory... if we want to change and updata the
+// value than it's original value is efficted... it's take the reference of original value 
