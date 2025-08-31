7a4f5110f3494209e11ebbec088e2b5494c2349e
Priyanka <145449555+Priyanka2351@users.noreply.github.com>
2025-08-31 18:26:28 +0000

    IIFE  immediately invoked function expressions

---
+++ basic2/iife.js
@@ -0,0 +1,10 @@
+// immediately invooked function expression
+(function hello(){
+    console.log("BD conected");    
+})();
+// this: is use for making two 
+// iife ({})()
+((name)  => {
+    ;console.log(  ` this the name ${name}` )
+})("psnjbhbds")
+
