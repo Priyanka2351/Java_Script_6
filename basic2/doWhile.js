d2b3c3ac4f287278bc944406c0529ffe73256f4f
Priyanka <145449555+Priyanka2351@users.noreply.github.com>
2025-09-16 09:58:02 +0000

    dowhile  while

---
+++ basic2/doWhile.js
@@ -0,0 +1,19 @@
+let num=0;
+while(num<=10){
+    console.log(num);
+    num+=2
+    
+}
+// 
+let arr=[2,1,44,5,66,6,7,7]
+let i=0;
+while( i<arr.length){
+    console.log(arr[i]);
+    i++
+}
+//do while loop
+let count =0
+do{
+    console.log("hello sir");
+    count+=2
+}while(count<=10);
